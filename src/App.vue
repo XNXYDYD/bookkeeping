ca2aabb0c4d940a210b68ead482ed7f096dc58e6
XNXYDYD <543179038@qq.com>
2019-03-19 10:13:54 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
+    <!-- tabbar -->
+    <div>
+      <van-tabbar v-model="current">
+        <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
+        <van-tabbar-item icon="search" to="./record">我的分析</van-tabbar-item>
+      </van-tabbar>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      /* tabbar current */
+      current:0
+    }
+  },
+  beforeCreate(){
+    let setRem = () => {
+      let _;(_ = function(){document.querySelector('html').style.fontSize = 0.0625*(750<=document.documentElement.clientWidth?750:document.documentElement.clientWidth)+"px";})();window.onresize = _;
+    }
+    // setRem();
   }
 }
 </script>
 
 <style>
+@import './common/css/cssreset.css';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body{
+  font-size:16px;
 }
 </style>
